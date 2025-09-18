fecaadc9b6fa05f724be60b67230bf3b4fee9467
just-fontaine <henrypmesquita@gmail.com>
2025-09-18 11:29:34 -0300

    docstrings adicionadas

--- main.py
+++ main.py
@@ -14,6 +14,19 @@ class Sprite:
         convert_alpha:      bool,
         topleft:            bool
     ) -> None:
+        """
+        Inicializa a classe Sprite
+
+        Args:
+            caminho_sprite (str): O caminho da imagem que será desenhada.
+            transform_scale (tuple[int, int]): As dimensões que serão redefinidas.
+            x (int): Coordenada X de onde o Sprite deverá ser desenhado.
+            y (int): Coordenada Y de onde o Sprite deverá ser desenhado.
+            convert_alpha (bool): Deverá ser True caso a imagem não tiver fundo.
+            topleft (bool): True caso a referência for o canto superior esquerdo, caso contrário será o centro.
+        Returns:
+            None
+        """
         self.caminho_sprite: str = caminho_sprite
 
         if convert_alpha:
@@ -32,6 +45,14 @@ class Sprite:
         self.y: int = y
     
     def desenhar(self, tela: pg.Surface) -> None:
+        """
+        Desenha o sprite na tela.
+
+        Args:
+            tela (pg.Surface): Superfície na qual será desenhado o Sprite.
+        Returns:
+            None
+        """
         tela.blit(self.imagem, self.rect)
 
 class Corpo:
@@ -44,6 +65,19 @@ class Corpo:
         aceleracao:         Vector2,
         sprite:             Optional[pg.Surface] = None
     ) -> None:
+        """
+        Inicializa a classe Corpo.
+
+        Args:
+            massa (float): Massa do corpo.
+            raio (float): Raio do corpo.
+            posicao (Vector2): Posição inicial do Corpo.
+            velocidade (Vector2): Velocidade inicial do Corpo.
+            aceleracao (Vector2): Aceleração inicial do Corpo.
+            sprite (Optional[pg.Surface]): Sprite do corpo.
+        Returns:
+            None
+        """
         self.massa: float           = massa
         self.raio: float            = raio
         self.posicao: Vector2       = posicao
@@ -55,11 +89,25 @@ class Corpo:
         self.rect: pg.Rect          = self.sprite_red.get_rect(center=(int(self.posicao.x), self.posicao.y))
 
     def desenhar(self, tela: pg.Surface) -> None:
+        """
+        Desenha o corpo na tela.
+
+        Args:
+            tela (pg.Surface): Superfície na qual será desenhado o Corpo.
+        Returns:
+            None
+        """
         self.rect.center = self.posicao
         tela.blit(self.sprite_red, self.rect)
 
 class Simulacao:
     def __init__(self) -> None:
+        """
+        Inicializa a classe Simulacao.
+
+        Returns:
+            None
+        """
         self.FRAMERATE: int = 100
 
         self.ROXO_ESCURO: tuple[int, int, int]    = (75, 0, 130)
@@ -132,16 +180,24 @@ class Simulacao:
         )
     
     def atualizar_projeteis(self, dt: float) -> None:
-        x_planeta: int          = self.planeta.posicao.x
-        y_planeta: int          = self.planeta.posicao.y
-        massa_planeta: float    = self.planeta.massa
+        """
+        Realiza os cálculos físicos e atualiza os projéteis a cada frame da simulação.
+
+        Args:
+            dt (float): Delta time
+        Returns:
+            None
+        """
+        x_planeta       = self.planeta.posicao.x
+        y_planeta       = self.planeta.posicao.y
+        massa_planeta   = self.planeta.massa
         if self.projeteis:
             for projetil in self.projeteis:
-                x_projetil: int         = projetil.posicao.x
-                y_projetil: int         = projetil.posicao.y
-                massa_projetil: float   = projetil.massa
+                x_projetil          = projetil.posicao.x
+                y_projetil          = projetil.posicao.y
+                massa_projetil      = projetil.massa
                 
-                distancia: int = (self.planeta.posicao - projetil.posicao).length()
+                distancia = (self.planeta.posicao - projetil.posicao).length()
 
                 if distancia < 1:
                     distancia = 1
@@ -180,6 +236,18 @@ class Simulacao:
         massa:          float,
         raio:           float
     ) -> None:
+        """
+        Adicina um projetil a lista de projeteis.
+
+        Args:
+            posicao (Vector2): Posição onde o projetil será desenhado.
+            velocidade (Vector2): Velocidade inicial do projetil.
+            aceleracao (Vector2): Aceleração inicial do projetil.
+            massa (float): Massa do projetil.
+            raio (float): Raio do projetil.
+        Returns:
+            None
+        """
         projetil = Corpo(
             massa=massa,
             posicao=posicao,
@@ -193,6 +261,12 @@ class Simulacao:
         self.velocidade_inicial_projetil += 5
 
     def desenhar_tudo(self) -> None:
+        """
+        Desenha tudo na tela.
+
+        Returns:
+            None
+        """
         self.fundo.desenhar(self.tela)
         self.planeta.desenhar(self.tela)
         self.torre.desenhar(self.tela)
@@ -202,6 +276,12 @@ class Simulacao:
             projetil.desenhar(self.tela)
     
     def escrever_textos(self) -> None:
+        """
+        Escreve todas as informações na tela.
+
+        Returns:
+            None
+        """
         self.escrever_texto(
             info=f'Projetil Atual: {len(self.projeteis)}',
             x=10,
@@ -242,6 +322,12 @@ class Simulacao:
             )
 
     def event_loop(self) -> None:
+        """
+        Verifica quais teclas foram apertadas e realiza ações de acordo com elas.
+
+        Returns:
+            None
+        """
         for evento in pg.event.get():
             if evento.type == pg.QUIT:
                 self.jogo_rodando = False
@@ -258,6 +344,12 @@ class Simulacao:
                     )
 
     def run(self) -> None:
+        """
+        Game Loop da simulação.
+
+        Returns:
+            None
+        """
         last_time = time.time()
         self.jogo_rodando = True
         while self.jogo_rodando:
@@ -273,12 +365,26 @@ class Simulacao:
             self.clock.tick(self.FRAMERATE)
     
     def escrever_texto(self, info: str, x: int, y: int):
+        """
+        Escreve um texto na tela.
+
+        Args:
+            info (str): String que será escrita na tela.
+            x (int): Coordenada X do texto.
+            y (int): Coordenada Y do texto.
+        """
         superficie_texto = self.fonte.render(str(info), True, self.VERDE)
         retangulo_texto = superficie_texto.get_rect(topleft=(x, y))
         pg.draw.rect(self.tela, 'Black', retangulo_texto)
         self.tela.blit(superficie_texto, retangulo_texto)
 
 def main() -> None:
+    """
+    Função principal que roda a simulação.
+
+    Returns:
+        None
+    """
     pg.init()
     simulacao = Simulacao()
     simulacao.run()
